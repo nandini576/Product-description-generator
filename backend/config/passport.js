18f2fc239908c51174f0f039c7ccbc6ebd61baf4
Nandini Kona <nandukona12@gmail.com>
2026-08-02 12:47:07 +0530

    Update OAuth callback URL

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -1,101 +1,50 @@
 import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import User from "../models/User.js";
-
-
 passport.use(
     new GoogleStrategy(
-
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
-
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-
             callbackURL:
-            "http://localhost:5000/api/auth/google/callback"
-
+            "https://product-description-generator-gt6k.onrender.com/api/auth/google/callback"
         },
-
-
         async(accessToken, refreshToken, profile, done)=>{
-
-
             try{
-
-
                 let user = await User.findOne({
                     googleId: profile.id
                 });
-
-
-
                 if(!user){
-
-
                     user = await User.findOne({
                         email:
                        profile.emails[0].value.toLowerCase()
                     });
-
-
                     if(user){
-
-
                         user.googleId = profile.id;
-
                         user.picture =
                         profile.photos?.[0]?.value || null;
-
-
                         await user.save();
-
-
                     }
 
                     else{
-
-
                         user = await User.create({
-
                             name:
                             profile.displayName,
-
-
                             email:
                             profile.emails[0].value,
-
-
                             googleId:
                             profile.id,
-
-
                             picture:
                             profile.photos?.[0]?.value || null
-
                         });
-
-
                     }
-
                 }
-
-
                 return done(null,user);
-
-
-
             }
             catch(error){
-
                 return done(error,null);
-
             }
-
-
         }
-
     )
 );
-
-
 export default passport;
